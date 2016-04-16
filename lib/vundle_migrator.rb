8ae1020d4190a06d7078fd34fd0d65e82587f372
Travis Yoder <trayoda@gmail.com>
2016-04-15 21:14:18 -0700

    migrator can prepend the line to source the plugins.vim

--- lib/vundle_migrator.rb
+++ lib/vundle_migrator.rb
@@ -7,7 +7,7 @@ module VundleMigrator
     attr_reader :plugins
 
     VIMRC_SOURCE = "\" load vundle plugins\n" \
-                   "source  ~/.vim/vundle/plugins.vim"
+                   "source  ~/.vim/vundle/plugins.vim\n\n"
 
     PLUGINS_START = "set nocompatible\n" \
                     "filetype off\n" \
@@ -17,39 +17,57 @@ module VundleMigrator
     PLUGINS_END = "\n\ncall vundle#end()\n\n" \
                   "filetype plugin indent on"
 
-    def initialize(source="#{Dir.home}/.vim/bundle", destination="#{Dir.home}/.vim/vundle")
-      @destination = destination
+    def initialize(vimrc, source="#{Dir.home}/.vim/bundle", destination="#{Dir.home}/.vim/vundle")
+      @vimrc = vimrc
       @source = source
+      @destination = destination
       @entries = Dir.entries(source) - %w(. ..)
       @plugins = []
     end
 
     def run
       create_plugins_list
+      create_vundle_folder
       create_plugins_file
+      prepend_vimrc
     end
 
-    private
+    def create_plugins_list
+      @plugins = @entries.map do |plugin_directory|
+        Dir.chdir("#{@source}/#{plugin_directory}") do
+          package = `git remote -v`.match(/com[\/|:](.+)\.git/)[1]
+          "Plugin '#{package}'"
+        end
+      end
+    end
 
-      def create_plugins_list
-        @entries.each do |plugin_directory|
-          Dir.chdir("#{@source}/#{plugin_directory}") do
-            package = `git remote -v`.match(/com[\/|:](.+)\.git/)[1]
-            @plugins << "Plugin '#{package}'"
-          end
+    def create_vundle_folder
+      FileUtils.mkdir_p(@destination)
+    end
+
+    def create_plugins_file
+      File.open("#{@destination}/plugins.vim", "w") do |f|
+        f.write(PLUGINS_START)
+
+        @plugins.each do |plugin|
+          f.write(plugin)
+        end
+
+        f.write(PLUGINS_END)
+      end
+    end
+
+    def prepend_vimrc
+      File.rename("#{@vimrc}", "#{@vimrc}.old")
+
+      File.open("#{@vimrc}", 'w') do |f|
+        f.puts VIMRC_SOURCE
+        File.foreach("#{@vimrc}.old") do |l|
+          f.puts l
         end
       end
 
-      def create_plugins_file
-        File.open("#{@destination}/plugins.vim", "w") do |f|
-          f.write(PLUGINS_START)
-
-          @plugins.each do |plugin|
-            f.write(plugin)
-          end
-
-          f.write(PLUGINS_END)
-        end
-      end
+      FileUtils.remove("#{@vimrc}.old")
+    end
   end
 end
--- test/vundle_migrator_test.rb
+++ test/vundle_migrator_test.rb
@@ -10,7 +10,15 @@ require 'fileutils'
 class VundleMigratorTest < Minitest::Test
   def setup
     @destination = "/vim/vundle"
-    @source = "vim/bundle"
+    @source = "/vim/bundle"
+    @vim_root = "/vim"
+    @vimrc = "#{@vim_root}/vimrc"
+  end
+
+  def teardown
+    FakeFS do
+      FileUtils.rm_r("vim") if File.file?("vim")
+    end
   end
 
   def test_that_it_has_a_version_number
@@ -18,34 +26,77 @@ class VundleMigratorTest < Minitest::Test
   end
 
   def test_it_can_create_a_list_of_plugins
+    setup_directories
+    plugin_list = "Plugin 'trayo/vundle_migrator'"
+
     FakeFS do
-      setup_directories
+      migrator = VundleMigrator::Migrator.new(@vim_root, @source, @destination)
 
-      migrator = VundleMigrator::Migrator.new(@source, @destination)
-      migrator.run
+      migrator.create_plugins_list
 
-      assert_equal migrator.plugins.first, "Plugin 'trayo/vundle_migrator'"
+      assert_equal plugin_list, migrator.plugins.first
+    end
+  end
+
+  def test_it_can_create_the_vundle_folder
+    setup_directories
+
+    FakeFS do
+      migrator = VundleMigrator::Migrator.new(@vim_root, @source, @destination)
+
+      migrator.create_vundle_folder
+
+      assert File.directory?(@destination), "#{@destination} wasn't created"
     end
   end
 
   def test_it_can_create_a_plugins_file
-    FakeFS do
-      setup_directories
-      vundle_begin = "call vundle#begin()"
-      vundle_end = "call vundle#end()"
+    setup_directories
+    vundle_begin = "call vundle#begin()"
+    vundle_end = "call vundle#end()"
 
-      migrator = VundleMigrator::Migrator.new(@source, @destination)
-      migrator.run
+    FakeFS do
+      migrator = VundleMigrator::Migrator.new(@vimrc, @source, @destination)
+
+      migrator.create_plugins_list
+      migrator.create_vundle_folder
+      migrator.create_plugins_file
 
       results = File.read("#{@destination}/plugins.vim")
 
-      assert results =~ /.*#{vundle_begin}.*#{migrator.plugins.first}.*#{vundle_end}/m,
-        "didn't find vundle commands '#{vundle_begin}' '#{migrator.plugins.first}' '#{vundle_end}' in: \n---\n#{results}\n---"
+      assert File.directory?(@destination), "#{@destination} wasn't created"
+      assert results =~ /.*#{vundle_begin}.*#{migrator.plugins.first}.*#{vundle_end}/m, "didn't find vundle commands '#{vundle_begin}' '#{migrator.plugins.first}' '#{vundle_end}' in: \n---\n#{results}\n---"
     end
   end
 
+  def test_it_can_prepend_to_the_vimrc
+    results = ""
+    final_vimrc = "source  ~/.vim/vundle/plugins.vim"
+    existing_vimrc = "some cool stuff"
+
+    FakeFS do
+      setup_directories
+      File.open(@vimrc, "w") do |f|
+        f.write(existing_vimrc)
+      end
+
+      migrator = VundleMigrator::Migrator.new(@vimrc, @source, @destination)
+      migrator.create_plugins_list
+      migrator.create_vundle_folder
+      migrator.create_plugins_file
+      migrator.prepend_vimrc
+
+      results = File.read(@vimrc)
+    end
+
+    assert results =~ /.*#{final_vimrc}.*#{existing_vimrc}/m, "expected vimrc:\n---\n#{final_vimrc}\n\n#{existing_vimrc}\n---\n\ndid not match: \n+++\n#{results}+++"
+  end
+
   def setup_directories
-    FileUtils.mkdir_p(@destination)
-    FileUtils.mkdir_p("#{@source}/test_plugin")
+    FakeFS do
+      FileUtils.mkdir_p("#{@source}/test_plugin")
+      FileUtils.mkdir_p(@vim_root)
+      FileUtils.touch(@vimrc)
+    end
   end
 end
